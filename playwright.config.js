908aebbd98e534850eb9f535e93ad5d84d237fc4
Jonny Gerig Meyer <jonny@oddbird.net>
2022-08-05 11:38:16 -0400

    node v18

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ export default {
   webServer: {
     command: 'LEVEL=warning npm run start',
     url: 'http://localhost:3000/',
+    timeout: 5 * 1000,
   },
   use: {
     baseURL: 'http://localhost:3000/',
